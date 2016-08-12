2dff723f6c804632f6113fa0de52107d5e46aaeb
Shenggang <hushenggang1996@163.com>
2016-08-12 15:42:27 +0100

    Some updates
    Found a package for enemy (provided by ZeroBudgetGames). Uploaded a
    rough design for the Threat system, start writing some script tomorrow.

---
+++ Code_Raid_Prototype_01/Assets/Script/FlowControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void LoadScene()
+    {
+        Application.LoadLevel("tCombat01");
+    }
+}
